231b6c7fa045b85f2d0d81c25edc30237166eb17
Sergio Martin Rubio <econsergio@gmail.com>
2020-06-21 19:57:55 +0100

    Initial Commit

---
+++ prime-factors-prime-numbers-calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  prime-factors-prime-numbers-calculator
+//
+//  Created by Sergio Martin Rubio on 21/06/2020.
+//  Copyright © 2020 Sergio Martin Rubio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
